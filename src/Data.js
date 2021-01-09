af708b52dd712c978e5395af1fc9a034797eab41
Aleem Tariq <aleemtariq19@gmail.com>
2021-01-09 15:19:03 -0800

    Removed data class, unnecessary, imports added.

--- src/Data.js
+++ src/Data.js
@@ -1,21 +1,17 @@
-class Data {
-    // reads the data specified from csv
-    constructor() {
-        
-    }
+import infectionData from "./data/covid19-data.json"
+import vaccineData from "./data/vaccine-distribution-data.json"
 
-    // Creates a local array based upon data read in constructor
-    // Returns specific information related to infectionData
-    returnInfectionData = () => {
+
+// Creates a local array based upon data read in constructor
+// Returns specific information related to infectionData
+returnInfectionData = () => {
         // pass
-    }
-
-    // Creates a local array based upon data read in constructor
-    // Returns specific information related to infectionData
-    returnVaccineData = () => {
-        // pass
-    }
-
-    // Additional functions...
-    }
 }
+
+// Creates a local array based upon data read in constructor
+// Returns specific information related to infectionData
+returnVaccineData = () => {
+        // pass
+    }
+
+// Additional functions 
