80db9df281890eed9b52ec74909cd980c07b7a38
Kristen <kristen.foong@gmail.com>
2021-01-09 15:52:08 -0800

    general header

--- src/App.js
+++ src/App.js
@@ -18,9 +18,21 @@ function App() {
           Learn React!
         </a>
       </header> */}
+      
       <div class="main">
         <div class="head">
-
+          <div class="side">
+            <div class="side-in">
+              A simple React app that takes public Covid data and displays it per user request. 
+              Created for nwHacks 2021 by our awesome team.
+            </div>
+          </div>
+          <div class="title">
+            COVID TRACKER
+          </div>
+          <div class="links">
+            <a href="link">Link</a>
+          </div>
         </div>
       </div>
     </div>
