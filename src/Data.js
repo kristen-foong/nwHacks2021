57e2c2764f3f8f08739ee96412d64cd91817fe6a
Aleem Tariq <aleemtariq19@gmail.com>
2021-01-09 14:36:20 -0800

    Basic testing, linked Data.js to dummy.html

--- src/Data.js
+++ src/Data.js
@@ -1,9 +1,9 @@
-var csv = require('./jquery.csv.js'); // Imported jquery csv library 
+console.log('Hello');
 
 class Data {
     // reads the data specified from csv
     constructor() {
-
+        console.log('Test')
     }
 
     // Creates a local array based upon data read in constructor
@@ -20,3 +20,5 @@ class Data {
 
     // Additional functions...
 }
+
+const p = new Data();
