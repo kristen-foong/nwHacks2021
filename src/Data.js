4ca7fb94433fa600ed712f7395ebd1a1cc932cb3
Aleem Tariq <aleemtariq19@gmail.com>
2021-01-09 14:00:39 -0800

    Added csv-reader, and framework for Data class.

---
+++ src/Data.js
@@ -0,0 +1,22 @@
+var csv = require('./jquery.csv.js'); // Imported jquery csv library 
+
+class Data {
+    // reads the data specified from csv
+    constructor() {
+
+    }
+
+    // Creates a local array based upon data read in constructor
+    // Returns specific information related to infectionData
+    returnInfectionData = () => {
+        // pass
+    }
+
+    // Creates a local array based upon data read in constructor
+    // Returns specific information related to infectionData
+    returnVaccineData = () => {
+        // pass
+    }
+
+    // Additional functions...
+}
