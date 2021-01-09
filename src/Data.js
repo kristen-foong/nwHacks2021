8678b5bc0a1a858b4c7f4035aefb51e8108dd446
Aleem Tariq <aleemtariq19@gmail.com>
2021-01-09 15:16:22 -0800

    Converted data files to json, undid some errors.

--- src/Data.js
+++ src/Data.js
@@ -1,9 +1,7 @@
-console.log('Hello');
-
 class Data {
     // reads the data specified from csv
     constructor() {
-        console.log('Test')
+        
     }
 
     // Creates a local array based upon data read in constructor
@@ -19,6 +17,5 @@ class Data {
     }
 
     // Additional functions...
+    }
 }
-
-const p = new Data();
