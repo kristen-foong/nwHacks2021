e3334091918165f3a2f1fd0367974b3b1b557607
Aleem Tariq <aleemtariq19@gmail.com>
2021-01-09 16:58:06 -0800

    Finished respective data functions.

--- src/Data.js
+++ src/Data.js
@@ -4,7 +4,7 @@ const vaccineData = require('./data/vaccine-distribution-data.json')
 // Creates a local array based upon data read in constructor
 // Returns specific information related to infectionData
 function returnInfectionData(provinceName) {
-    dayANDinfection = [] // arrays of (day, infection)
+    dayANDinfection = [] // arrays of ((day, infection)...)
 
     var size = Object.keys(infectionData).length; // find size of JSON object
     var i; // loop iterator
@@ -14,7 +14,7 @@ function returnInfectionData(provinceName) {
             var singlePoint = [];
             singlePoint.push(infectionData[i].date); // get date
             singlePoint.push(infectionData[i].numconf); // get confirmedCases
-            dayANDinfection.push(singlePoint); // add to array of (day, infection)
+            dayANDinfection.push(singlePoint); // add to array
         }
     }
     return dayANDinfection;     
@@ -22,13 +22,21 @@ function returnInfectionData(provinceName) {
 
 // Creates a local array based upon data read in constructor
 // Returns specific information related to infectionData
-// returnVaccineData(provinceName) {
-//     dayANDvaccines = []; // array of (day, vaccines distributed)
+function returnVaccineData(provinceName) {
+    dayANDvaccines = []; // array of ((day, vaccines distributed)...)
 
-//     var size = Object.keys(vaccineData).length; // find size of JSON object
-//     var i; // loop iterator
-// }
+    var size = Object.keys(vaccineData).length; // find size of JSON object
+    var i; // loop iterator
+    for (i=0; i < size; i++) {
+        if (vaccineData[i].province = provinceName) {
+            // create array to store (day, vaccines distributed)
+            var singlePoint = [];
+            singlePoint.push(vaccineData[i].date_vaccine_distributed); // get date
+            singlePoint.push(vaccineData[i].cumulative_dvaccine); // get distributions
+            dayANDvaccines.push(singlePoint); // add to array
+        }
+    }
+    return dayANDvaccines;
+}
 
 // Additional functions
-
-console.log(returnInfectionData("Yukon"));
