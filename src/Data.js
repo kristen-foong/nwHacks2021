2d48f9c691f7fef8ea4e49ecffb92139d9bdf281
Aleem Tariq <aleemtariq19@gmail.com>
2021-01-09 16:31:00 -0800

    Added returnInfectionData function.

--- src/Data.js
+++ src/Data.js
@@ -1,11 +1,23 @@
-import infectionData from "./data/covid19-data.json"
-import vaccineData from "./data/vaccine-distribution-data.json"
-
+const infectionData = require('./data/covid19-data.json')
+const vaccineData = require('./data/vaccine-distribution-data.json')
 
 // Creates a local array based upon data read in constructor
 // Returns specific information related to infectionData
-returnInfectionData = () => {
-        // pass
+function returnInfectionData(provinceName) {
+    dayANDinfection = [] // arrays of (day, infection)
+
+    var size = Object.keys(infectionData).length; // find size of JSON object
+    var i; // loop iterator
+    for (i=0; i < size; i++) {
+        if (infectionData[i].prname == provinceName) {
+            // create array to store (day, infection)
+            var singlePoint = []
+            singlePoint.push(infectionData[i].date); // get date
+            singlePoint.push(infectionData[i].numconf); // get confirmedCases
+            dayANDinfection.push(singlePoint); // add to array of (day, infection)
+        }
+    }
+    return dayANDinfection;     
 }
 
 // Creates a local array based upon data read in constructor
@@ -15,3 +27,5 @@ returnVaccineData = () => {
     }
 
 // Additional functions 
+
+console.log(returnInfectionData("British Columbia"));
